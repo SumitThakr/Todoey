e3676e68b756b6afceb96afdb39a4cfe85d49a6e
SumitThakr <sumit.thk@gmail.com>
2019-09-27 15:34:54 +0530

    Convert to MVC design pattern

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by abc on 9/27/19.
+//  Copyright © 2019 abc. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title: String = ""
+    var done: Bool = false
+}
